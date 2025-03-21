82e8c4137121d051d046276d0ad28d8754cb95ae
SoloScriptSage <vladyslavhirchukbusiness@outlook.com>
2025-03-21 22:42:40 +0100

    Read feature vector implementation

--- ml101/data_handler.cc
+++ ml101/data_handler.cc
@@ -12,7 +12,30 @@ DataHandler::~DataHandler() {
 	// FREE DYNAMIC MEMORY
 }
 
-void DataHandler::read_feature_vector(string path);
+void DataHandler::read_feature_vector(string path) {
+	uint32_t header[4]; // MAGIC | NUM IMAGES| ROWSIZE | COLSIZE
+	unsigned char bytes[4];
+	
+	FILE* file = fopen(path.c_str(), "rb");
+	if (file) {
+		for (int i = 0; i < 4; ++i) {
+			if (fread(bytes, sizeof(bytes), 1, file)) {
+				header[i] = count_to_little_endian(bytes);
+			}
+		}
+		printf("Done getting file header\n");
+
+		int image_size = header[2] * header[3];
+		for (int i = 0; i < header[1]; ++i) {
+			unsigned char* image = new unsigned char[image_size];
+			if (fread(image, sizeof(unsigned char), image_size, file)) {
+				Data* data = new Data(image, image_size);
+				data_array->push_back(data);
+			}
+		}
+	}
+}
+
 void DataHandler::read_feature_labels(string path);
 
 void DataHandler::split_data();
